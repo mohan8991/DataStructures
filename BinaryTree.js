0bdb0346c5fd693ffb8eb94b8a934e86b330c12a
mohan.gopikrishna <mohan.gopikrishna@servicenow.com>
2020-03-07 20:44:23 -0500

    working node insert

--- BinaryTree.js
+++ BinaryTree.js
@@ -21,15 +21,31 @@ class BinaryTree {
     }
 
     insertNode(node, newNode){
-        if(newNode.data < node){
-            node.left = newNode;
-        } else if (newNode.data > node) {
-            node.right = newNode;
+        console.log(newNode)
+        console.log(newNode.data < node.data)
+        if(newNode.data < node.data){
+            if(node.left===null)
+                node.left = newNode;
+            else {
+                this.insertNode(node.left, newNode)
+            }
+        } else if (newNode.data > node.data) {
+            if(node.right === null)
+                node.right = newNode;
+            else {
+                this.insertNode(node.right, newNode)
+            }
         }
-
     }
 
     search(){}
     traverse(){}
     remove(){}
 }
+
+let tree = new BinaryTree();
+tree.insert(4);
+tree.insert(2);
+tree.insert(5);
+
+console.dir(tree)
