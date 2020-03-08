4fcd936cc184daab56dcaa815fb3c41abd5b105f
mohan.gopikrishna <mohan.gopikrishna@servicenow.com>
2020-03-07 20:09:55 -0500

    binary structure implementation

---
+++ BinaryTree.js
@@ -0,0 +1,20 @@
+class Node {
+    constructor(data){
+        this.data = data;
+        this.right=null;
+        this.left=null;
+    }
+}
+
+class BinaryTree {
+    constructor(){
+        this.root = null;
+    }
+
+    insert(){}
+    search(){}
+    traverse(){}
+    remove(){}
+}
+
+
